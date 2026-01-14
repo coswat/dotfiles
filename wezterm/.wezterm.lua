dad6eb51306250cd85802451f24a7ccd10b0e3f1
swat <abhishek.b4696@gmail.com>
2026-01-15 00:44:09 +0530

    new keybindings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
@@ -7,9 +7,35 @@ config.font_size = 15
 config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = "catppuccin-mocha"
 config.colors = { background = "#171717" }
-
-
+config.leader = { key = "Space", mods = "SHIFT" }
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.enable_tab_bar = false
+
+-- Toggle opacity
+wezterm.on("toggle-opacity", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+
+	if not overrides.window_background_opacity then
+		overrides.window_background_opacity = 0.66
+	else
+		overrides.window_background_opacity = nil
+	end
+
+	window:set_config_overrides(overrides)
+end)
+
+-- Key Bindings
+config.keys = {
+	{
+		key = "O",
+		mods = "LEADER",
+		action = wezterm.action.EmitEvent("toggle-opacity"),
+	},
+	{
+		key = "T",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+}
 
 return config
